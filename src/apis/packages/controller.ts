ee1a21ae742437d8941b69d74544c0bfe4123e62
Negasi-Kifle <kiflenegasi@yahoo.com>
2023-12-22 09:55:04 +0300

    Added APIs to fetch packages in tenant and whole DB

--- src/apis/packages/controller.ts
+++ src/apis/packages/controller.ts
@@ -4,7 +4,7 @@ import PackageDAL from "./dal";
 import slugifer from "../../utils/slugfier";
 import School from "../schools/dal";
 import IUsersDoc from "../users/dto";
-import check_ownership from "../students/utils/check_ownership";
+import checkOwnership from "../students/utils/check_ownership";
 
 // Create package
 export const createPack: RequestHandler = async (req, res, next) => {
@@ -15,7 +15,7 @@ export const createPack: RequestHandler = async (req, res, next) => {
 
     // Check the logged in user owns/belongs to the school
     const loggedInUser = <IUsersDoc>req.user;
-    check_ownership(loggedInUser, school);
+    checkOwnership(loggedInUser, school);
 
     // Incoming data
     const data = <PmtPackageRequests.ICreatePmtPackage>req.value;
@@ -32,6 +32,49 @@ export const createPack: RequestHandler = async (req, res, next) => {
       status: "SUCCESS",
       message: "New package creates successfully",
       data: { package: pack },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get all payment packages in a tenant
+export const getTenantPackages: RequestHandler = async (req, res, next) => {
+  try {
+    const school = await School.getSchool(req.params.tenantId);
+    if (!school) return next(new AppError("School does not exist", 404));
+
+    // Check the logged in user owns/belongs to the school
+    const loggedInUser = <IUsersDoc>req.user;
+    checkOwnership(loggedInUser, school);
+
+    // Get packages
+    const pmtPackages = await PackageDAL.getTenantPackages(
+      school.id,
+      req.query
+    );
+
+    // Response
+    res.status(200).json({
+      status: "SUCCESS",
+      results: pmtPackages.length,
+      data: { pmtPackages },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+// Get all packages in DB
+export const getAllPackagesInDB: RequestHandler = async (req, res, next) => {
+  try {
+    const pmtPackages = await PackageDAL.getAllPackagesInDB(req.query);
+
+    // Response
+    res.status(200).json({
+      status: "SUCCESS",
+      results: pmtPackages.length,
+      data: { pmtPackages },
     });
   } catch (error) {
     next(error);
